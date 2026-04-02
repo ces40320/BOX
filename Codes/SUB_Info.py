f67e2c6235d00f1ecd619ca2bbc16f161ca36739
ces40320 <ces40320@gmail.com>
2026-04-02 19:20:46 +0900

    Add: SUB_Info.py
    A file containing basic information about the research participants organised into a dictionary immediately after the experiment.
    
    Based on this file, all directory and path names, as well as the processing methods, for the subsequent processing code are determined.

---
+++ Codes/SUB_Info.py
@@ -0,0 +1,95 @@
+"""
+피험자 정보 및 실험 설정 (전체 Analysis 공용)
+  - 모든 분석 과정에정 호출되어 사용됨
+
+각 피험자 항목에 포함되는 필드(dict key-value pairs):
+"yymmdd_NAMECODE" : {
+        "SUB_number"         : (int) 피험자 번호 숫자만 (e.g. 1),
+        "body_mass"          : (float) 체중 (kg),
+        "height"             : (int) 신장 (mm),
+        "sex"                : (str) 성별 ("M" / "F"),
+        "age"                : (int) 만 나이,
+        "protocol"           : (str) 프로토콜 이름 (e.g. "Symmetric" / "Asymmetric_Pilot" / "Asymmetric_Triangle" ),
+        "conditions"         : {"7kg_10bpm":  {"order":      (int) 프로토콜 순서 (1 ~ 4),
+                                               "cycles":     (int) 반복 횟수,
+                                               "error_log":  (str in list) 에러 로그 (e.g. []),
+                                            },
+                                "15kg_10bpm": {"order":      (int) 프로토콜 순서 (1 ~ 4),
+                                              "cycles":      (int) 반복 횟수,
+                                              "error_log":   (str in list) 에러 로그 (e.g. ["1AB", "4CA", "11AB"]),
+                                            },
+                                "7kg_16bpm":  {"order":      (int) 프로토콜 순서 (1 ~ 4),
+                                               "cycles":     (int) 반복 횟수,
+                                               "error_log":  (str in list) 에러 로그 (e.g. ["1AB", "7BC"]),
+                                            }, 
+                                "15kg_16bpm": {"order":      (int) 프로토콜 순서 (1 ~ 4),
+                                               "cycles":     (int) 반복 횟수,
+                                               "error_log":  (str in list) 에러 로그 (e.g. ["4CA", "1AB", "11AB"]),
+                                            },
+                            },
+    },
+"""
+
+subjects = {
+    
+    "260303_MJY" : { #문주영
+        "SUB_number"         : 1,
+        "body_mass"          : 71.58,
+        "height"             : 1770,
+        "sex"                : "F",
+        "age"                : 23,
+        "protocol"           : "Asymmetric_Triangle",
+        "conditions"         : {"7kg_10bpm": {"order":       1,
+                                              "cycles":      11,
+                                              "error_log":   ['1AB'],
+                                            },
+                                "15kg_10bpm": {"order":      3,
+                                               "cycles":     10,
+                                               "error_log":  [],
+                                            }, 
+                                "7kg_18bpm": {"order":       2,
+                                              "cycles":      12,
+                                              "error_log":   ['1AB','5AB'],
+                                            },
+                                "15kg_18bpm": {"order":      4,
+                                               "cycles":     11,
+                                               "error_log":  ['1AB', '7AB', '11AB'],
+                                            },
+                            },
+    },
+    
+    "260306_KTH" : { #김태영
+        "SUB_number"         : 2,
+        "body_mass"          : 66.74,
+        "height"             : 1730,
+        "sex"                : "M",
+        "age"                : 22,
+        "protocol"           : "Asymmetric_Triangle",
+        "conditions"         : {"7kg_10bpm":  {"order":      1,
+                                               "cycles":     11,
+                                               "error_log":  ['7AB'],
+                                            },
+                                "15kg_10bpm": {"order":      2,
+                                              "cycles":      11,
+                                              "error_log":   ['4CA'],
+                                            },
+                                "7kg_16bpm":  {"order":      4,
+                                               "cycles":     10,
+                                               "error_log":  [],
+                                            }, 
+                                "15kg_16bpm": {"order":      3,
+                                               "cycles":     10,
+                                               "error_log":  [],
+                                            },
+                            },
+    },
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
